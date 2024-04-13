7507a03b512305ca02920066719ee0b3c2172684
johnyDriotis <johny.driotis1996@gmail.com>
2024-04-13 13:57:56 -0600

    Servir ficheros estaticos, es decir html, imagenes, archivos css, javscript etc

--- FriendsApp/FriendsApp/Program.cs
+++ FriendsApp/FriendsApp/Program.cs
@@ -1,6 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Se agrega servicio para controladores y vistas junto con RazorRuntimeCompilation para cambios en caliente
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Se define pagina de excepcion para ambiente desarrollo
+if (app.Environment.IsDevelopment())
+{
+	app.UseDeveloperExceptionPage();
+}
+
+// Permite que nos muestre por defecto los archivos estaticos. toma por defecto el archivo html llamada default.html
+app.UseDefaultFiles();
+
+// Habilita el uso de archivos estaticos
+app.UseStaticFiles();
+
+// Se agrega la ruta por defecto para la aplicacion.
+//app.MapDefaultControllerRoute();
 
 app.Run();
